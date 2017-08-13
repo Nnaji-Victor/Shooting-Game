1a79bb63810af149254c4481544cd757a08824c2
OrionStar25 <chunnushrivastava@gmail.com>
2017-08-13 17:15:04 +0530

    restart buttona nd timer added

--- js/game.js
+++ js/game.js
@@ -1,16 +1,10 @@
-var left_key = 37;
-var up_key = 38;
-var right_key = 39;
-var down_key = 40;
-var counter = 0; 
-
 function makeDiv(){
     // vary size for fun
     var divsize = ((Math.random()*100) + 50).toFixed();
     var options = ['#f39c12','#f33412','#12f3a5','#1231f3','#86079f','#1ad20e','#dcf90f'];
     var color = options[Math.floor(Math.random()*7)];
    
-    $newdiv = $('<div/>').css({
+    $newdiv = $('<div class="new"/>').css({
         'width':divsize+'px',
         'height':divsize+'px',
         'border-radius': '50%',
@@ -27,25 +21,64 @@ function makeDiv(){
         'top':posy+'px',
         'display':'none'
     }).appendTo( 'body' ).fadeIn(100).click(function() {
-    	$(this).fadeOut(200, function(){
-    			$(this).remove();
-    			counter += 1;
-    			$('#counter').text("Score: " + counter);
-      			makeDiv();    		
-    	}); 
-	});	
+        $(this).fadeOut(200, function(){
+                $(this).remove();
+                counter += 1;
+                $('#counter').text("Score: " + counter);            
+        });
+        makeDiv(); 
+    }); 
 }
 
-setInterval(function () {
-    var d = new Date(); //get current time
-    var seconds = d.getMinutes() * 60 + d.getSeconds(); //convet current mm:ss to seconds for easier caculation, we don't care hours.
-    var oneMin = 60; 
-    var timeleft = oneMin - seconds % oneMin; // let's say now is 01:30, then current seconds is 60+30 = 90. And 90%300 = 90, finally 300-90 = 210. That's the time left!
-    var result = parseInt(timeleft / 60) + ':' + timeleft % 60; //formart seconds back into mm:ss 
-    document.getElementById('test').innerHTML = "Time left: " + result;
+function createTimer(seconds) {
+    intervalVar = setInterval(function () {
+    makeDiv();
+    if (seconds === 0) {
+        $('#context').text(seconds);
+        clearInterval(intervalVar);
+        $('#context').text("Time's up!");
+        alert("Your score is " + counter);
+        endGame();
+        return;
+    }
+    if (seconds <= 10 && seconds > 5) {
+        document.getElementById("context").style.color = "yellow";
+    }
+    else if (seconds <= 5) {
+        document.getElementById("context").style.color = "red";
+    }
+    else {
+        document.getElementById("context").style.color = "white";
+    }
+    var minutes = Math.floor(seconds / 60);
+    var secondsToShow = (seconds - minutes * 60).toString();
+    if (secondsToShow.length === 1) {
+        secondsToShow = "0" + secondsToShow; // if the number of seconds is '5' for example, make sure that it is shown as '05'
+    }
+    $('#context').text(minutes.toString() + ":" + secondsToShow);
+    seconds--;
+    }, 1000);
+}
 
-}, 500) //calling it every 0.5 second to do a count down
+var counter;
+
+function initNewGame(){
+    counter = 0; 
+    $('#restart').hide();
+    createTimer(60);
+}
+
+function endGame() {
+    //remove divs
+    $('.new').remove();
+    //put a restart button, which when clicked calls newgame
+    $('#restart').fadeIn('fast',function(){
+        $(this).click(function() {
+            initNewGame();
+        });
+    });
+}
 
 $(document).ready(function() {
-	makeDiv();
+	initNewGame();
 });
